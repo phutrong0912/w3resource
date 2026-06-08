48ca1e929019974dfb278f75bf16e60786dcdadf
phutrong0912 <trongpnp.23ceb@vku.udn.vn>
2026-06-08 15:55:29 +0700

    Add file program Basic 37 & 38

---
+++ Basic/Basic_37.cpp
@@ -0,0 +1,35 @@
+/*Write a C++ program to print a mystery series from 1 to 50.
+
+Sample Output:
+Print a mystery series:
+-------------------------
+The series are:
+5 4 2 7 11 10 8 13 17 16 14 19 23 22 20 25 29 28 26 31 35 34 32 37 41 40 38 43 47 46 44 49*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n=1,a=10,i;
+	
+	/*while (true) {
+		++n;
+		if ((n%3)==0) 
+			continue;
+		if (n==a)
+			break;
+		if ((n%2)==0) 
+			n+=3;
+		else n-=3;
+		cout<<n<<" ";
+		} */
+	for	(i=1;i<=a;i++){
+		++n;
+		if((n%3)==0)
+			continue;
+		if((n%2)==0) 
+			n+=3;
+		else n-=3;
+		cout<<n<<" ";
+	}
+}
---
+++ Basic/Basic_38.cpp
@@ -0,0 +1,29 @@
+/*Write a C++ program that takes a number as input and prints its multiplication table up to 10.
+
+Sample Output:
+Print the multiplication table of a number upto 10:
+--------------------------------------------------------
+Input a number: 5
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+5 x 10 = 50*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n,i;
+	cin>>n;
+	
+	for (i=1;i<=10;i++) 
+		cout<<n*i<<endl;
+	
+	return 0;
+}
